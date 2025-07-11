27e9fe488c74fcb859303b9ddfd9ab9ffd32aef1
Manish-p21 <manishpanigrahi.official@gmail.com>
2025-07-11 13:34:21 +0530

    ui update2

--- src/components/HomeSection/PropertyList.js
+++ src/components/HomeSection/PropertyList.js
@@ -152,7 +152,7 @@ const PropertyList = () => {
       </Swiper>
 
       {/* Property Grid */}
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-12">
         {properties.map((property, index) => (
           <div
             key={index}
